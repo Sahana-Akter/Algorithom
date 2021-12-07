085ae53e1faf749ea01714db1485230d0c80e3f1
Sahana-Akter <78800802+Sahana-Akter@users.noreply.github.com>
2021-12-07 13:19:16 +0600

    Add files via upload

---
+++ DFS_Recursive.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+vector<ll> adjlist[10];//2d array vector  10 size means 10 rows vector
+ll vis[10];
+void DFS(ll source)
+{
+    if (vis[source] != -1)
+        return;
+    vis[source]=0;//visited
+    for (int i = 0; i < adjlist[source].size(); i++)
+    {
+        DFS(adjlist[source][i]);
+
+    }
+}
+int main()
+{
+    memset(vis, -1, sizeof(vis));
+    return 0;
+}
